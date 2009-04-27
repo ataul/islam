6f7630b1d689b3316dda0572173ea0c453d359f9
ataul.haque <ataul.haque@e944697f-ad24-0410-bdd6-c9548301e4d7>
2009-04-27 10:13:41 +0000

    default init.php

--- website/init.php
+++ website/init.php
@@ -1,5 +1,4 @@
 <?
-$host="libra.creashhosting.com";
-mysql_connect($host,"creasha_islam","admin");
-@mysql_select_db(creasha_yusuf) or die( "Unable to select database");
+mysql_connect("localhost","root","");
+@mysql_select_db("quran_bangla") or die( "Unable to select database");
 ?>
